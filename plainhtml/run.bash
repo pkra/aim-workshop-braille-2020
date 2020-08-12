7a4ababdab9c52ea4ebf856b4d8357b4c6436f90
Phil Schatz <253202+philschatz@users.noreply.github.com>
2020-08-12 10:17:22 -0500

    use a custom filename
    "html.sem" was conflicting with the builtin file at /usr/share/liblouisutdml/lbu_files/html.sem

--- plainhtml/run.bash
+++ plainhtml/run.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -x
+set -e
 
 for filename in $(ls *.in.html); do
   input="${filename%.*}"
   base="${input%.*}"
   brf="${base}.out.brf"
   roundtrip="${base}.out.round"
-  file2brl --html --config-file "html.cfg" "${filename}" > "${brf}"
+  file2brl --html --config-file "_liblouis_html.cfg" "${filename}" > "${brf}"
   file2brl --backward "${brf}" > "${roundtrip}"
 done
