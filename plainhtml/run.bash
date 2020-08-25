1c3967eb74e2dcb3ce0adfe2e20b6804dde0cd64
philschatz <253202+philschatz@users.noreply.github.com>
2020-08-25 13:37:33 -0500

    use pretex file as starting point for the HTML

--- plainhtml/run.bash
+++ plainhtml/run.bash
@@ -7,6 +7,6 @@ for filename in $(ls *.in.html); do
   base="${input%.*}"
   brf="${base}.out.brf"
   roundtrip="${base}.out.round"
-  file2brl --html --config-file "_liblouis_html.cfg" "${filename}" > "${brf}"
+  file2brl --config-file "_plainhtml.cfg" "${filename}" > "${brf}"
   file2brl --backward "${brf}" > "${roundtrip}"
 done
